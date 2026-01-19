97a38cc51b49792014cb8528a53ee07cac6c6432
Nadia <nyadiia@pm.me>
2026-01-18 18:55:04 -0600

    Remove 1Password installation from build.sh
    Removed 1Password repository installation from build script.

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,17 +12,11 @@ fi
 echo "Enabling Terra repository..."
 dnf5 config-manager setopt terra.enabled=1
 
-echo "Installing 1Password repository..."
-rpm --import https://downloads.1password.com/linux/keys/1password.asc
-sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
-
 echo "Installing extra programs..."
 dnf5 install -y --skip-unavailable --skip-broken \
   ripgrep \
   fd-find \
   bat \
-  1password \
-  1password-cli \
   direnv \
   zoxide \
   git-delta \
